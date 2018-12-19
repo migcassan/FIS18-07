c541d9bdb0649f938d5bf55e3f8d0187064a7622
Faustino ayala <favsol26@gmail.com>
2018-12-19 20:45:32 +0100

    fixed

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
-
+var BASE_API_PATH = "/api/v1";
 var app = express();
 
 app.get("/", (req, res) => {
     res.send("<html><body><h1>My server</h1></body></html>");
+});
+
+app.get(BASE_API_PATH + "/contacts", (req, res) => {
+
+    console.log(Date() + " - GET /contacts");
+
+    res.send(__dirname);
+
 });
 
 module.exports.app = app;
